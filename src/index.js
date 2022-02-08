dcabaac8c37832d80bb8aa363863e380cfbdb073
Shami <shamitomita@gmail.com>
2022-02-07 20:30:29 -0500

    commit tree 68fe3860b61dccbef01592d8f1654f914aa94f70

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleClick= () =>{
+
+}
 ReactDOM.render(
   <div>
     <SimpleComponent />
